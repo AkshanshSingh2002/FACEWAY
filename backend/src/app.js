950a154d82e3161d1e3d362454f377ef8268276e
Akshansh Singh <akshanshs2002@gmail.com>
2025-06-23 23:40:09 +0530

    added .env

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 import express from 'express';
 import { createServer } from 'node:http';
 // import { Server } from 'socket.io';
@@ -19,7 +21,7 @@ app.use(express.urlencoded({extended: true, limit: '40kb'}));
 app.use('/api/v1/users', userRoutes);
 
 const start = async () => {
-    const connectionDB = await mongoose.connect('mongodb+srv://akshanshs2000learn:akshansh2000@cluster0.g3kca6j.mongodb.net/');
+    const connectionDB = await mongoose.connect(process.env.MONGO_URI);
     console.log(`Connected to MongoDB: ${connectionDB.connection.host}`);
     server.listen(app.get("port"), () => {
         console.log('Server is running on port 8000');
